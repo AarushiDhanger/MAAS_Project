8c23d1b8ffb72faff7b88d80ca497c144f72c576
Andrew Emerson <32277829+AndrewDEmerson@users.noreply.github.com>
2022-04-16 21:26:29 -0700

    Fixed env setup problems.

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,6 @@
 :: Run this script to set up your virtual enviroment for the project
 :: Works on Windows
-@echo on
+@echo off
 if not EXIST %0\..\weather\configurations.ini (
     echo no config file detected
     exit /b 0
@@ -9,15 +9,16 @@ if not EXIST %0\..\finalprojectvenv\ (
     echo Creating Python VENV
     cd %0\..
     py -3 -m venv finalprojectvenv
-    .\finalprojectvenv\Scripts\activate.bat
+    call %0\..\finalprojectvenv\Scripts\activate.bat
     pip install django
     echo Created VENV
     echo Establishing Database
-    %0\..\finalprojectvenv\Scripts\activate.bat
     cd %0\..\weather
     python manage.py migrate
     echo done creating database
+    cd %0\..
 )
-if sys.prefix == sys.base_prefix (%0\..\finalprojectvenv\Scripts\activate.bat)
-cd %0\..\weather
+if not defined VIRTUAL_ENV  (
+    call %0\..\finalprojectvenv\Scripts\activate.bat)
+cd %~f0\..\weather
 python manage.py runserver
