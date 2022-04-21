da17236d089907713f6450cba1d543816c7ab729
Andrew Emerson <32277829+AndrewDEmerson@users.noreply.github.com>
2022-04-20 17:29:28 -0700

    Get Hourly data from openweather.
    Using the onecall endpoint allows for more data including current and
    hourly temps as well as sunrise sunset.

--- weather/main/views.py
+++ weather/main/views.py
@@ -15,7 +15,6 @@ def index(request):
 
     if request.method == 'POST':
         city = request.POST['city']
-        print("User Lat: %s\t Lon: %s"%(request.POST['clientlatitude'],request.POST['clientlongitude']))       
          
         #find the Lattitude and Longitude of the searched location
         try:
@@ -25,14 +24,16 @@ def index(request):
         except HTTPError as error:
             eprint.error("Received HTTPError from openstreetmap api call: %d" %(error.code))
             raise NotImplementedError
-        list_of_data = json.loads(source)
-        print("Searched Location Lat: %s\t Lon: %s"%(list_of_data[0]['lat'],list_of_data[0]['lon']))
+        map_data = json.loads(source)
+        if len(map_data) == 0:
+            #No Location was found for the search request; figure out a nice way to handle this.
+            raise NotImplementedError
            
         # Get weather information at the searched location, Using coordinates found by OSM
         try:
             source = urllib.request.urlopen(
-            'http://api.openweathermap.org/data/2.5/weather?lat=' 
-                    + urllib.parse.quote(list_of_data[0]['lat']) + '&lon=' + urllib.parse.quote(list_of_data[0]['lon']) + '&appid=' + config.apikey).read()
+            'https://api.openweathermap.org/data/2.5/onecall?lat=' 
+                    + urllib.parse.quote(map_data[0]['lat']) + '&lon=' + urllib.parse.quote(map_data[0]['lon']) + '&appid=' + config.apikey + '&units=imperial').read()
         except HTTPError as error:
             eprint.error("Received HTTPError from openweathermap api call: %d" %(error.code))
             if error.code == 404:
@@ -43,20 +44,19 @@ def index(request):
         
         # converting JSON data to a dictionary
         list_of_data = json.loads(source)
-        print(great_circle((request.POST['clientlatitude'],request.POST['clientlongitude']), (list_of_data['coord']['lat'],list_of_data['coord']['lon'])).km)
+        #print(great_circle((request.POST['clientlatitude'],request.POST['clientlongitude']), (list_of_data['coord']['lat'],list_of_data['coord']['lon'])).km)
         # data for variable list_of_data
         data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ' '
-                        + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + 'k',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-            "user_coordinate": request.POST['clientlongitude'] + ' '
-                        + request.POST['clientlatitude'],
-            "distance":str(great_circle((request.POST['clientlatitude'],request.POST['clientlongitude']), (list_of_data['coord']['lat'],list_of_data['coord']['lon'])).miles)
+            "location_title":str(map_data[0]['display_name']),
+            "coordinate": str(list_of_data['lat']) + ', '
+                        + str(list_of_data['lon']),
+            "temp": str(list_of_data['current']['temp']) + 'F',
+            "pressure": str(list_of_data['current']['pressure']),
+            "humidity": str(list_of_data['current']['humidity']),
+            "user_coordinate": request.POST['clientlatitude'] + ', '
+                        + request.POST['clientlongitude'],
+            "distance":str(round(great_circle((request.POST['clientlatitude'],request.POST['clientlongitude']), (list_of_data['lat'],list_of_data['lon'])).miles,2)) + ' mi'
         }
-        print(data)
     else:
         data ={}
     return render(request, "main/index.html", data)
