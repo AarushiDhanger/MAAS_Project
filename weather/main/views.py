cdee6df375245eb874d8504d5780a63f616298c5
Andrew Emerson <32277829+AndrewDEmerson@users.noreply.github.com>
2022-04-30 20:49:26 -0700

    Make the website look better.
    Changed website color scheme.
    Changed website layout and background.
    Added comments on weather.
    Display weather image loaded from openweather.
    Fixed bug in autofilling the current time.

--- weather/main/views.py
+++ weather/main/views.py
@@ -1,4 +1,7 @@
+import datetime
+import time;
 from logging import exception
+from math import floor
 from urllib.error import HTTPError
 from django.shortcuts import render
 
@@ -12,12 +15,17 @@ import config
 from eprint import eprint
 from geopy.distance import great_circle
 def index(request):
-
+    #try:
     if request.method == 'POST':
         city = request.POST['city']
-        time = request.POST['time']
-        print(time)
-         
+        #Convert time the user gave into the UTC time zone that the weather api uses.
+        usertime = request.POST['time']
+        offset = request.POST['clienttimeoffset']
+        splittime = usertime.split(':');
+        utctime = str((int(splittime[0]) + floor(int(offset)/60))%24) + ":" + str((int(splittime[1])+int(offset))%60)
+        current_time = datetime.datetime.now() 
+        unixtime = int(time.mktime(datetime.datetime(current_time.year, current_time.month, current_time.day, int(splittime[0]) if int(splittime[1]) < 30 else int(splittime[0]) + 1, 0).timetuple()))
+        
         #find the Lattitude and Longitude of the searched location
         try:
             source = urllib.request.urlopen(
@@ -30,7 +38,7 @@ def index(request):
         if len(map_data) == 0:
             #No Location was found for the search request; figure out a nice way to handle this.
             raise NotImplementedError
-           
+        
         # Get weather information at the searched location, Using coordinates found by OSM
         try:
             source = urllib.request.urlopen(
@@ -46,18 +54,53 @@ def index(request):
         
         # converting JSON data to a dictionary
         list_of_data = json.loads(source)
+        
+        settemp = None;
+        icon = None;
+        for n in list_of_data["hourly"]:
+            if n["dt"] == unixtime:
+                settemp = n["temp"]
+                icon = str(n['weather']).split('\'')[13]
+                eprint.info("timematch found with temp: ",settemp)
+                break
+        if settemp == None:
+            eprint.error("Failed to find the temperature for the time given, reverting to current time")
+            settemp = list_of_data['current']['temp']
+            icon = str(list_of_data['current']['weather']).split('\'')[13]
+        
+        weathercomment = None;
+        if settemp > 100:
+            weathercomment = "Make sure that your AC is working!"
+        elif settemp > 80 :
+            if int(splittime[0]) > 7 and int(splittime[0]) < 18:
+                weathercomment = "You might want to wear sunscreen, It's hot outside!"
+            else:
+                weathercomment = "You might not get sunburn, but you'll still feel its heat!"
+        elif settemp > 70:
+            weathercomment = "Its a nice day outside, be sure to enjoy it!"
+        elif settemp > 60:
+            weathercomment = "The weather is average, a good day to get work done!"
+        elif settemp > 45:
+            weathercomment = "The weather is a bit chilly, be sure to bring a jacket!"
+        else:
+            weathercomment = "It's very cold! Watch out"
+
         data = {
-            "location_title":str(map_data[0]['display_name']),
+            "location_title":str(map_data[0]['display_name']).split(',')[0],
             "coordinate": str(list_of_data['lat']) + ', '
                         + str(list_of_data['lon']),
-            "temp": str(list_of_data['current']['temp']) + 'F',
+            "temp": str(settemp) + 'F',
             "pressure": str(list_of_data['current']['pressure']),
             "humidity": str(list_of_data['current']['humidity']),
             "user_coordinate": request.POST['clientlatitude'] + ', '
                         + request.POST['clientlongitude'],
-            "distance":str(round(great_circle((request.POST['clientlatitude'],request.POST['clientlongitude']), (list_of_data['lat'],list_of_data['lon'])).miles,2)) + ' mi',
-            "time":str(time)
+            "distance":str(round(great_circle((request.POST['clientlatitude'],request.POST['clientlongitude']), (list_of_data['lat'],list_of_data['lon'])).miles,2)),
+            "comment":weathercomment,
+            "imageid":icon,
         }
     else:
         data ={}
     return render(request, "main/main.html", data)
+    #except:
+    #    return render(request, "main/404.html")
+
